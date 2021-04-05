e5ea48911559e5f7b99b3b25a3c8eab0328fa174
joebfigg <joebfigg@hotmail.com>
2021-04-05 15:00:05 -0700

    addDragable

--- scripts/script.js
+++ scripts/script.js
@@ -175,3 +175,35 @@ element4.classList.remove("subPurple3","subYellow3");
 }
 
 
+
+/* drag */
+
+$(function(){
+
+$("#drag").css({"position":"absolute","top":"100px","right":"100px","border":"1px solid #789","cursor":"move"})
+  
+	var dragging = false;
+	var iX, iY;
+	$("#drag").mousedown(function(e) {
+		dragging = true;
+		iX = e.clientX - this.offsetLeft;
+		iY = e.clientY - this.offsetTop;
+		this.setCapture && this.setCapture();
+		return false;
+	});
+	document.onmousemove = function(e) {
+		if (dragging) {
+			var e = e || window.event;
+			var oX = e.clientX - iX;
+			var oY = e.clientY - iY;
+			$("#drag").css({"left":oX + "px", "top":oY + "px"});
+			return false;
+		}
+	};
+	$(document).mouseup(function(e) {
+		dragging = false;
+		$("#drag")[0].releaseCapture();
+		e.cancelBubble = true;
+	})
+})
+
