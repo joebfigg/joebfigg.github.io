557828f2d3e3730547a6936d4e2a0c047b160f3d
joebfigg <joebfigg@hotmail.com>
2021-04-04 20:10:26 -0700

    wix styles

--- scripts/script.js
+++ scripts/script.js
@@ -32,3 +32,84 @@ $(function() {
   }
 
 });
+
+
+function displaySwitch() {
+
+var element = document.getElementById("wix1");
+
+    if (element.classList.contains("d-none")) {
+      element.classList.add("d-block");
+      element.classList.remove("d-none");
+    } else {
+      element.classList.add("d-none");
+      element.classList.remove("d-block");
+    }
+    
+}
+
+function yellowTheme() {
+
+var element1 = document.getElementById("hero");
+var element2 = document.getElementById("wix1");
+var element3 = document.getElementById("wix2");
+var element4 = document.getElementById("wix3");
+
+element1.classList.add("headYellow");
+element1.classList.remove("headRed","headPurple");
+
+element2.classList.add("subYellow1");
+element2.classList.remove("subRed1","subPurple1");
+
+element3.classList.add("subYellow2");
+element3.classList.remove("subRed2","subPurple2");
+    
+element4.classList.add("subYellow3");
+element4.classList.remove("subRed3","subPurple3");     
+    
+}
+
+function purpleTheme() {
+
+var element1 = document.getElementById("hero");
+var element2 = document.getElementById("wix1");
+var element3 = document.getElementById("wix2");
+var element4 = document.getElementById("wix3");
+
+element1.classList.add("headPurple");
+element1.classList.remove("headRed","headYellow");
+
+element2.classList.add("subPurple1");
+element2.classList.remove("subRed1","subYellow1");
+
+element3.classList.add("subPurple2");
+element3.classList.remove("subRed2","subYellow2");
+    
+element4.classList.add("subPurple3");
+element4.classList.remove("subRed3","subYellow3");      
+    
+}
+
+
+function redTheme() {
+
+var element1 = document.getElementById("hero");
+var element2 = document.getElementById("wix1");
+var element3 = document.getElementById("wix2");
+var element4 = document.getElementById("wix3");
+
+element1.classList.add("headRed");
+element1.classList.remove("headPurple","headYellow");
+
+element2.classList.add("subRed1");
+element2.classList.remove("subPurple1","subYellow1");
+
+element3.classList.add("subRed2");
+element3.classList.remove("subPurple2","subYellow2");
+    
+element4.classList.add("subRed3");
+element4.classList.remove("subPurple3","subYellow3");    
+    
+}
+
+
